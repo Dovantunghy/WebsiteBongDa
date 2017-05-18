0926d2a44fe80b111665a90883a1338d00ac3ec6
Dovantunghy <dovantunghy186@gmail.com>
2017-05-19 00:05:45 +0700

    Add project 2

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Đồ_án_website_Bóng_đá.Startup))]
+namespace Đồ_án_website_Bóng_đá
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
